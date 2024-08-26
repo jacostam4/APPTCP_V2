bc6c81072abfeeaceaeffa12eedc73e0c66596d5
jacostam4 <juanacostamahecha@gmail.com>
2024-08-26 16:07:39 -0500

    Commit inicial

---
+++ src/main/java/com/mycompany/apptcpjcam/APPTCPJCAM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apptcpjcam;
+
+/**
+ *
+ * @author Sala_706
+ */
+public class APPTCPJCAM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
